f02ce39c6557a31b9182b0f3a87cab032323d1fc
rumabe97 <rumabe97@gmail.com>
2022-07-07 09:02:15 +0200

    Firebase deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: '334909e3a101971bd6273ede7043d4e1',
+  privateKey: '17f75e46fd6a456afa3fa09374ae059de8408e62',
+  url: 'https://gateway.marvel.com:443/v1/public/'
 };
