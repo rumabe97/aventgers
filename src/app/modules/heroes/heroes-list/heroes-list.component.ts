dc66a8988c959b3bf8b9ed2157d888d309182734
rumabe97 <rumabe97@gmail.com>
2022-07-29 14:27:16 +0200

    Add search hero

--- src/app/modules/heroes/heroes-list/heroes-list.component.ts
+++ src/app/modules/heroes/heroes-list/heroes-list.component.ts
@@ -2,8 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {IList} from "../../../shared/entities/list";
 import {MarvelService} from "../../../core/services/marvel/marvel.service";
-import {LoadingService} from "../../../core/services/loading/loading.service";
-import {delay} from "rxjs";
+import {FormControl, FormGroup} from "@angular/forms";
+import {Subject, switchMap} from "rxjs";
 
 @Component({
     selector: 'app-heroes-list',
@@ -12,23 +12,53 @@ import {delay} from "rxjs";
 })
 export class HeroesListComponent implements OnInit {
     data!: IList;
+    search: FormGroup
+    searchValue: string = '';
+
+    scroll: boolean = false;
+    query: any = {};
+
+    private reactionsTrigger$ = new Subject<void>();
 
     constructor(private _route: ActivatedRoute, private _marvelService: MarvelService) {
     }
 
+
     ngOnInit(): void {
         this.data = this._route.snapshot.data['response'];
+        this.initSubject();
+        this.initSearchGroup();
+    }
+
+    initSearchGroup() {
+        this.search = new FormGroup({
+            inputSearch: new FormControl()
+        });
+        this.search.controls['inputSearch'].valueChanges.subscribe(value => {
+            this.searchValue = value;
+            this.query = {offset: 0};
+            if (this.searchValue) this.query['nameStartsWith'] = this.searchValue;
+            this.scroll = false;
+            this.reactionsTrigger$.next();
+        })
+    }
+
+    private initSubject(): void {
+        this.reactionsTrigger$.pipe(
+            switchMap(() => this._marvelService.getAllHeroes(this.query)),
+        ).subscribe(value => this.data = {
+            ...value,
+            results: this.scroll ? this.data.results.concat(value.results) : value.results
+        });
+
     }
 
     onScroll(event: any) {
-        // visible height + pixel scrolled >= total height
         if (event.target.offsetHeight + event.target.scrollTop >= event.target.scrollHeight) {
-            this._marvelService.getAllHeroes({offset: this.data.offset + 20}).subscribe(value => {
-                this.data = {
-                    ...value,
-                    results: this.data.results.concat(value.results)
-                }
-            })
+            this.query = {offset: this.data.offset + 20};
+            if (this.searchValue) this.query['nameStartsWith'] = this.searchValue;
+            this.scroll = true;
+            this.reactionsTrigger$.next();
         }
     }
 }
--- src/app/modules/heroes/heroes.module.ts
+++ src/app/modules/heroes/heroes.module.ts
@@ -4,6 +4,7 @@ import {CommonModule} from '@angular/common';
 import {HeroesRoutingModule} from './heroes-routing.module';
 import {SharedModule} from "../../shared/shared.module";
 import {HeroesListComponent} from "./heroes-list/heroes-list.component";
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
@@ -12,6 +13,7 @@ import {HeroesListComponent} from "./heroes-list/heroes-list.component";
         CommonModule,
         HeroesRoutingModule,
         SharedModule,
+        ReactiveFormsModule,
     ]
 })
 export class HeroesModule {
